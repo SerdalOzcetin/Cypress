0f4f426dcb31c312caa1f1ebc8736bfdd92b5201
Serdal <ozcetinserdal@gmail.com>
2023-08-27 15:11:18 +0300

    iframes

---
+++ cypress/e2e/Frames.cy.js
@@ -0,0 +1,42 @@
+import 'cypress-iframe'  // after installing the "npm install -D cypress-iframe" you need to import that.!!
+
+describe('handling frames',()=>{
+
+    it("Approach01",()=>{
+
+        cy.visit("https://the-internet.herokuapp.com/iframe");
+
+        const iframe = cy.get("#mce_0_ifr")
+        .its('0.contentDocument.body')
+        .should('be.visible')
+        .then(cy.wrap);
+
+        iframe.clear().type("welcome {ctrl+a}");
+        cy.get("button[title='Bold']").click();
+
+    })
+
+    it("Approach02--by using custom method",()=>{
+
+        cy.visit("https://the-internet.herokuapp.com/iframe");
+
+        cy.getIframe("#mce_0_ifr").clear().type("welcome {ctrl+a}");
+        cy.get("button[title='Bold']").click();
+
+    })
+
+    it("Approach03--by using cypress-iframe plugin",()=>{
+
+        // https://www.npmjs.com/package/cypress-iframe
+        // npm install -D cypress-iframe
+
+        cy.visit("https://the-internet.herokuapp.com/iframe");
+        cy.frameLoaded("#mce_0_ifr"); // load the frame
+        cy.iframe("#mce_0_ifr").clear().type("welcome {ctrl+a}");
+        cy.get("button[title='Bold']").click();
+
+        
+
+    })
+
+})
